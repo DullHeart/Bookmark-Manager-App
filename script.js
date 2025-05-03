695793ced8b680bf051430ce1f8b2c036ed3030b
Leonard  Gnani <leonardgnani@MacBook-Air-von-Leonard.local>
2025-05-03 20:43:08 +0200

    Add Html and Css for the project

---
+++ script.js
@@ -0,0 +1,15 @@
+const categoryDropdown = document.getElementById('category-dropdown');
+const viewCategoryButton  = document.getElementById('view-category-button');
+const addBookmarkButton = document.getElementById('add-bookmark-button');
+const name = document.getElementById('name');
+const url = document.getElementById('url');
+const closeFormButton = document.getElementById('close-form-button');
+const addBookmarkButtonForm = document.getElementById('add-bookmark-button-form');
+const bookmarkListSection = document.getElementById('bookmark-list-section');
+const closeListButton = document.getElementById('close-list-button');
+const deleteBookmarkButton = document.getElementById('delete-bookmark-button');
+const selectContainer = document.querySelector("select");
+
+const getBookmarks = () => {
+  
+}
